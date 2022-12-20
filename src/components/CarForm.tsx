439f058fc7c31c194dd044897f0add9a36827802
Folderas <82723806+marcinwolder@users.noreply.github.com>
2022-12-20 01:37:41 +0100

    css

--- src/components/CarForm.tsx
+++ src/components/CarForm.tsx
@@ -11,37 +11,44 @@ export const CarForm: React.FC = () => {
 	}));
 	const handleSubmit = (e: FormEvent) => {
 		e.preventDefault();
-		dispatch(
-			addCar({
-				name,
-				cost,
-				id: nanoid(),
-			})
-		);
+		if (name && cost)
+			dispatch(
+				addCar({
+					name,
+					cost,
+					id: nanoid(),
+				})
+			);
 	};
 	return (
-		<form>
-			<label htmlFor='carName'>Car name:</label>
-			<input
-				type='text'
-				value={name}
-				onChange={(e) => {
-					dispatch(setName(e.target.value));
-				}}
-				name='carName'
-			/>
-			<label htmlFor='carCost'>Car cost:</label>
-			<input
-				type='number'
-				value={cost || ''}
-				onChange={(e) => {
-					dispatch(setCost(parseFloat(e.target.value)));
-				}}
-				name='carCost'
-			/>
-			<button onClick={handleSubmit} type='submit'>
-				Add car
-			</button>
-		</form>
+		<div className='bg-emerald-200 p-3 my-3 w-3/4 mx-auto'>
+			<form className='flex flex-col'>
+				<label htmlFor='carName'>Car name:</label>
+				<input
+					type='text'
+					value={name}
+					onChange={(e) => {
+						dispatch(setName(e.target.value));
+					}}
+					name='carName'
+				/>
+				<label htmlFor='carCost'>Car cost:</label>
+				<input
+					type='number'
+					value={cost || ''}
+					onChange={(e) => {
+						dispatch(setCost(parseFloat(e.target.value)));
+					}}
+					name='carCost'
+				/>
+				<button
+					className='mt-3 w-max mx-auto bg-white hover:bg-zinc-100 px-6 py-2 rounded-lg border-2 shadow-md border-zinc-400'
+					onClick={handleSubmit}
+					type='submit'
+				>
+					Add car
+				</button>
+			</form>
+		</div>
 	);
 };
--- src/components/CarList.tsx
+++ src/components/CarList.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState, removeCar } from '../store';
 
+import { FaTrashAlt } from 'react-icons/fa';
+
 export const CarList: React.FC = () => {
 	const data = useSelector((state: RootState) => state.cars.data);
 	const dispatch = useDispatch();
@@ -12,15 +14,22 @@ export const CarList: React.FC = () => {
 				dispatch(removeCar(car.id));
 			};
 			return (
-				<div key={car.id}>
-					<div className='flex justify-between'>
+				<div
+					key={car.id}
+					className='flex justify-between gap-3 p-2 shadow rounded border-zinc-600 border'
+				>
+					<div className='flex justify-between flex-1'>
 						<p>{car.name}</p>
 						<p>{car.cost}$</p>
 					</div>
-					<button onClick={handleRemove}>Remove</button>
+					<button onClick={handleRemove}>
+						<FaTrashAlt className='text-red-600 hover:text-red-700' />
+					</button>
 				</div>
 			);
 		});
 	};
-	return <div>{listElements()}</div>;
+	return (
+		<div className='w-1/2 mx-auto flex flex-col gap-2'>{listElements()}</div>
+	);
 };
