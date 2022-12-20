c8c7e277a553dfda9b33022a274aa2f492fbd6b4
Folderas <82723806+marcinwolder@users.noreply.github.com>
2022-12-20 10:29:26 +0100

    app done

--- src/components/CarList.tsx
+++ src/components/CarList.tsx
@@ -5,13 +5,16 @@ import { RootState, removeCar } from '../store';
 import { FaTrashAlt } from 'react-icons/fa';
 
 export const CarList: React.FC = () => {
-	const data = useSelector((state: RootState) => {
+	const { data, name } = useSelector((state: RootState) => {
 		const {
 			cars: { data, searchValue },
 		} = state;
-		return data.filter((car) =>
-			car.name.toLowerCase().includes(searchValue.toLowerCase())
-		);
+		return {
+			data: data.filter((car) =>
+				car.name.toLowerCase().includes(searchValue.toLowerCase())
+			),
+			name: state.form.name,
+		};
 	});
 	const dispatch = useDispatch();
 
@@ -20,12 +23,15 @@ export const CarList: React.FC = () => {
 			const handleRemove = () => {
 				dispatch(removeCar(car.id));
 			};
+			const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
 			return (
 				<div
 					key={car.id}
 					className='flex justify-between gap-3 p-2 shadow rounded border-zinc-600 border'
 				>
-					<div className='flex justify-between flex-1'>
+					<div
+						className={`flex justify-between flex-1 ${bold && 'font-medium'}`}
+					>
 						<p>{car.name}</p>
 						<p>${car.cost}</p>
 					</div>
